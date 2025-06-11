ea34889a88eddf8db0862ad6e8c3a1e16ef06e69
EvaSissy <sissyeva99@gmail.com>
2025-06-11 22:04:53 +0300

    release 110625

--- src/widgets/Clue.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import { getRiddleContext } from '../entities/riddle/context.ts'
-
-const { clue } = getRiddleContext()
-</script>
-
-<template>
-<h2>{{ clue }}</h2>
-</template>
